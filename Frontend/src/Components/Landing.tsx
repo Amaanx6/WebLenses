d0b61062778c1d2e90f518f65e815e978c6ad87b
Mohfazam <mohd.sarwar.code@gmail.com>
2025-03-21 15:49:33 +0530

    Initial Commit

---
+++ Frontend/src/Components/Landing.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Landing = () => {
+  return (
+    <div className='flex justify-center items-center h-screen w-screen'>
+      <div className='h-10 w-10 text-red-700 text-2xl'>
+      Hey
+      </div>
+    </div>
+  )
+}
+
